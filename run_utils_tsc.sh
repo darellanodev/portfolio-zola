82ae9d639d829a05eded89bf3cd1f1a4d86038de
darellanodev <darellano.aliaga@gmail.com>
2024-06-02 11:47:58 +0200

    rename scripts

--- run_utils_tsc.sh
+++
@@ -1,2 +0,0 @@
-npx tsc
-cp -r D:\\xampp\\htdocs\\proyectos\\portfolio-zola\\utils\\playable_demos\\templates D:\\xampp\\htdocs\\proyectos\\portfolio-zola\\utils\\dist\\playable_demos
--- run_utils_update_demos.sh
+++ run_utils_update_demos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./run_utils_tsc.sh
+npx tsc
+cp -r D:\\xampp\\htdocs\\proyectos\\portfolio-zola\\utils\\playable_demos\\templates D:\\xampp\\htdocs\\proyectos\\portfolio-zola\\utils\\dist\\playable_demos
 node ./utils/dist/playable_demos/main.js
 cp -r ./utils/playable_demos/templates ./utils/dist/playable_demos/
