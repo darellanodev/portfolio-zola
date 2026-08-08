33ccb4df370f430d53ace324ad04f239a8d9f114
darellanodev <darellano.aliaga@gmail.com>
2026-08-08 13:44:24 +0200

    chore: improve run.sh script with a menu

--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
-#!/bin/bash
-./zola.exe serve
+#!/usr/bin/env bash
+set -e
+
+# Ask the user
+echo "Select action:"
+echo "  1) run"
+echo "  2) build and copy to darellanodev.github.io"
+read -p "Option [1-2]: " PAIR
+
+# Main program
+case "$PAIR" in
+  1)
+    ./zola.exe serve
+    ;;
+  2)
+    ./run_build.sh && ./run_copy_build_except_demos.sh
+    ;;
+  *)
+    echo "Error: Incorrect option"
+    exit 1
+    ;;
+esac
--- run_build_and_copy_except_demos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./zola.exe build && ./run_copy_build_except_demos.sh
